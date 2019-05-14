59321951222a0adad5458b85e92b6f6fc8a7583e
Harri Ahola <L8613@student.jamk.fi>
2019-05-14 21:24:48 +0300

    Fixed issue related to missing Control annotation

--- src/main/java/fi/jamk/course/Bookstore/web/BookController.java
+++ src/main/java/fi/jamk/course/Bookstore/web/BookController.java
@@ -3,11 +3,12 @@ package fi.jamk.course.Bookstore.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
+@Controller
 public class BookController {
     @RequestMapping(value="/index", method=RequestMethod.GET)
     public String greetings(Model model) {
