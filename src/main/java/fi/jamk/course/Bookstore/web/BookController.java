505e8097f5c9f234c12d80843941eea17168d242
Harri Ahola <L8613@student.jamk.fi>
2019-05-15 21:24:39 +0300

    Chapter3, exercise2: Bookstore: Listpage

--- src/main/java/fi/jamk/course/Bookstore/web/BookController.java
+++ src/main/java/fi/jamk/course/Bookstore/web/BookController.java
@@ -3,16 +3,28 @@ package fi.jamk.course.Bookstore.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import fi.jamk.course.Bookstore.domain.BookRepository;
+
 @Controller
 public class BookController {
+	@Autowired
+	private BookRepository repository;
+	
     @RequestMapping(value="/index", method=RequestMethod.GET)
     public String greetings(Model model) {
         return "index";
+    }
+    
+    @RequestMapping(value="/booklist", method=RequestMethod.GET)
+    public String BookList(Model model) {
+    	model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
 	
 }
