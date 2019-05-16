355ce4e8cde01d7698898aa3bc6e023e44e5d3f6
Harri Ahola <L8613@student.jamk.fi>
2019-05-16 18:44:37 +0300

    Chapter3, exercise3: Bookstore: Crud

--- src/main/java/fi/jamk/course/Bookstore/web/BookController.java
+++ src/main/java/fi/jamk/course/Bookstore/web/BookController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import fi.jamk.course.Bookstore.domain.Book;
 import fi.jamk.course.Bookstore.domain.BookRepository;
 
 @Controller
@@ -26,5 +28,23 @@ public class BookController {
     	model.addAttribute("books", repository.findAll());
         return "booklist";
     }
+    
+    @RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbook";
+    }     
+    
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+} 
+    
 	
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
+    } 
 }
