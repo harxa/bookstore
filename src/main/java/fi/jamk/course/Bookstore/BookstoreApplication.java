ffdd0a9f21a4ad4ce450c715658d7466e5d46dea
Harri Ahola <L8613@student.jamk.fi>
2019-05-29 19:37:34 +0300

    Chapter6, exercise2: Bookstore: Securing the web application Part II

--- src/main/java/fi/jamk/course/Bookstore/BookstoreApplication.java
+++ src/main/java/fi/jamk/course/Bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import fi.jamk.course.Bookstore.domain.Book;
 import fi.jamk.course.Bookstore.domain.BookRepository;
 import fi.jamk.course.Bookstore.domain.Category;
 import fi.jamk.course.Bookstore.domain.CategoryRepository;
+import fi.jamk.course.Bookstore.domain.User;
+import fi.jamk.course.Bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -18,7 +20,7 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demoAdd(BookRepository repository, CategoryRepository crepository) {
+	public CommandLineRunner demoAdd(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 		return(args) ->{
 			
 			crepository.save(new Category("Dekkari"));
@@ -30,6 +32,14 @@ public class BookstoreApplication {
 			
 			repository.save(b1);
 			repository.save(b2);
+			
+			// Create users: user/user, useri/useri, admin/admin 
+			User user1 = new User("user", "$2a$10$UBii/e.Wy3sx5HiWgpfj7edVYR/e/zTuAoKP5bHZeFJcRe7WsIEz6", "user@email.me", "USER");
+			User user2 = new User("useri", "$2a$10$ti4io24XeSDi8WSbEAl6Ce1L/7zuA0rpId3jCiRmNZm2jxMcFGmRG", "useri@email.me", "USER");
+			User user3 = new User("admin", "$2a$10$SzTnZOy1xTkAKGb4aItDDuPEgJcyGF8q4mIXDAYSvhvLRGb/Q7gqG", "admin@email.me", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
+			urepository.save(user3);
 		};
 	}
 	
